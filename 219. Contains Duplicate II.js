8d0bb071846ca9713647bdd11e35dd58c0d142b0
jaggu0000 <sreejaganj@gmail.com>
2025-01-01 19:42:54 +0530

    Added new file

---
+++ 219. Contains Duplicate II.js
@@ -0,0 +1,33 @@
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+// Example 2:
+
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+// Example 3:
+
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+    const hashmap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (i - hashmap.get(nums[i]) <= k) {
+            return true;
+        }
+        hashmap.set(nums[i], i);
+    }
+    return false;
+};
