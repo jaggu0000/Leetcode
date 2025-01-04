cbeb8fd435c68b49b51a924c72342f53f8cce618
jaggu0000 <sreejaganj@gmail.com>
2025-01-04 15:10:01 +0530

    Added new code of time complexity O(N)

--- 412. Fizz Buzz.js
+++ 412. Fizz Buzz.js
@@ -39,3 +39,8 @@ var fizzBuzz = function (n) {
     return answer;
 };
 
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = (n) => new Array(n).fill(0).map((a, i) => (++i % 3 === 0 ? "Fizz" : "") + (i % 5 === 0 ? "Buzz" : "") || `${i}`)
