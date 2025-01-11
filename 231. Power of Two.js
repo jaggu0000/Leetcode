da5524b8e6674c6bb01e33c5f35cf65b79ea0270
jaggu0000 <sreejaganj@gmail.com>
2025-01-11 22:35:35 +0530

    Added a new approach

--- 231. Power of Two.js
+++ 231. Power of Two.js
@@ -40,3 +40,12 @@ var isPowerOfTwo = function(n) {
     }
     return true;
 };
+
+
+// A different approach
+
+var isPowerOfTwo = function (n) {
+    let x = 0
+    while (2 ** x < n) x++
+    return 2 ** x === n
+};
